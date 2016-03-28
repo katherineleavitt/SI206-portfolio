959d816e5e4da89ba44997760c0b58799a8d4cb1
katherineleavitt <ksleavit@umich.edu>
2016-03-27 22:58:40 -0400

    Initial Commit with responsive colors added

---
+++ main.py
@@ -0,0 +1,21 @@
+import webapp2
+import os
+import logging
+import jinja2
+
+# Lets set it up so we know where we stored the template files
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.info(self.request.path)
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        self.response.write(template.render())
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
