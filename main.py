6a07ffa99c4b92a07c03b361cd303840e82eb505
katherineleavitt <ksleavit@umich.edu>
2016-04-04 00:28:19 -0400

    updated templates/handlers

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import webapp2
 import os
 import logging
@@ -9,19 +25,34 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MainHandler(webapp2.RequestHandler):
+class HomeHandler(webapp2.RequestHandler):
     def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/home.html')
+        self.response.write(template.render({'subtitle': 'HOME'}))
 
-        try:
-            logging.info(self.request.path)
-            template = JINJA_ENVIRONMENT.get_template('templates/' + self.request.path)
-            self.response.write(template.render())
-        except:
-            logging.info(self.request.path)
-            template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-            self.response.write(template.render())
+class EducationHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/education.html')
+        self.response.write(template.render())
+
+
+class WorkHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/work.html')
+        self.response.write(template.render())
+
+class ContactHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/contact.html')
+        self.response.write(template.render())
+
+        
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/.*', MainHandler)
+    ('/', HomeHandler),
+    ('/home', HomeHandler),
+    ('/education', EducationHandler),
+    ('/work', WorkHandler),
+    ('/contact', ContactHandler),
 ], debug=True)
